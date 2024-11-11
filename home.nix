e035c9032bd2328f4a89dde44382e15322026a02
Ryan Swift <ryanswif@usc.edu>
2024-11-10 17:27:07 -0800

    added xclip to program list

--- home.nix
+++ home.nix
@@ -76,6 +76,7 @@
     openconnect
     ookla-speedtest
     signal-desktop
+    xclip
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
